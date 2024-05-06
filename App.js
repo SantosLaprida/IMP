2c28b41a039af81733c3d004c98ebd548ac8513e
Carlos Laprida <89549449+Charlie1203@users.noreply.github.com>
2024-05-06 17:56:07 -0300

    estilos log in

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Registro from './Registro'; // Importa el componente Registro desde registro.js
+import Registro from './Register'; // Importa el componente Registro desde registro.js
 
 const Stack = createStackNavigator();
 
@@ -33,13 +33,12 @@ function LoginScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Welcome to the Internet Match Play</Text>
-      </View>
-      
-     
+    <View style={styles.container2}>
+      <View >
+        <Text  style={styles.text}>Welcome to the Internet Match Play</Text>
+      </View>     
       <TextInput
-        style={styles.input}
+        style={{...styles.input, marginTop: "3em"}}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
@@ -51,24 +50,48 @@ function LoginScreen({ navigation }) {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <View style={{marginTop:10}}>
-        <Button title="Login" onPress={handleLogin} color="#841584"/> 
+      <View  style={{...styles.btn, marginTop: "2em"}}>
+        <Button  title="Login" onPress={handleLogin} /> 
       </View>
-      <View style={{marginTop:10}}> 
-        <Button title="Register" onPress={() => navigation.navigate('Registro')} color="#841584"/>
+      <View  style={styles.btn}> 
+        <Button  color={"green"}  title="Register" onPress={() => navigation.navigate('Registro')} />
       </View>
       
       <StatusBar style="auto" />
+    </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
+    height: '100vh',
+    width: '100vw',
+    backgroundImage: 'url("./assets/fondo.jpg")',
+    paddingBottom: 40,
+    paddingTop: 40,
+    backgroundSize: 'cover',
     flex: 1,
-    backgroundColor: '#ADD8E6', // Light blue
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  container2: {
+    borderRadius: '10px',
+    width: '90%',
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.788)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+   
+  },
+  text:{
+    color: 'white',
+    fontSize: '1.5rem',
+    fontFamily: "Roboto",
+    textAlign: 'center'
   },
   input: {
     height: 40,
@@ -76,14 +99,24 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginTop: 10,
     width: '80%',
+    color: "white",
     padding: 10,
+    borderRadius: "10px",
+    fontFamily: "Roboto",
   },
   loginText:{
     fontWeight: 'bold',
     fontSize: 20,
-    color: '#333',
+    color: 'white',
+    fontFamily: "Roboto",
+  },
+  btn:{
+    width: "80%",  
+    marginTop: 10,
+    backgroundColor: "black",
+    borderRadius: "10px",
+    fontFamily: "Roboto",
   }
-
 
 
 });
