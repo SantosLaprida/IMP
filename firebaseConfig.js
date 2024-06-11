161f0b6409ef9f6c3259fe488e32eedcc0bb4e82
SantosLaprida <lapridasantos95@gmail.com>
2024-06-11 16:41:48 -0600

    Initial firebase setup

---
+++ firebaseConfig.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAXzPEQIqJA9mBoamIjsfAyptIjgpMyo1o",
+  authDomain: "internetmatchplay.firebaseapp.com",
+  projectId: "internetmatchplay",
+  storageBucket: "internetmatchplay.appspot.com",
+  messagingSenderId: "1030174523087",
+  appId: "1:1030174523087:web:00f3a877aabb57628981ba",
+  measurementId: "G-B7Q070NLJ3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const firestore = getFirestore(app);
+
+export { firestore };
---
+++ scripts/importToFirestore.js
@@ -0,0 +1,57 @@
+const admin = require('firebase-admin');
+const fs = require('fs');
+
+// Path to your service account key
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const firestore = admin.firestore();
+
+// Function to import data from a JSON file into Firestore
+const importData = async (jsonFilePath, collectionName) => {
+  const data = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+  let batch = firestore.batch();
+  let count = 0;
+
+  for (let index = 0; index < data.rows.length; index++) {
+    const item = data.rows[index];
+    const docRef = firestore.collection(collectionName).doc();
+    batch.set(docRef, item);
+    count++;
+
+    // Commit batch every 500 operations
+    if ((index + 1) % 500 === 0) {
+      try {
+        await batch.commit();
+        console.log(`Batch committed at index ${index + 1}`);
+      } catch (error) {
+        console.error(`Error committing batch at index ${index + 1}`, error);
+      }
+      batch = firestore.batch(); // Reinitialize the batch
+    }
+  }
+
+  // Commit the remaining batch
+  try {
+    await batch.commit();
+    console.log('Final batch committed');
+    console.log(`Total documents processed: ${count}`);
+  } catch (error) {
+    console.error('Error committing final batch', error);
+  }
+  console.log(`Data successfully imported to ${collectionName}`);
+};
+
+// Import data for I_Torneos
+const main = async () => {
+  try {
+    await importData('I_Torneos.json', 'I_Torneos');
+  } catch (error) {
+    console.error('Error importing data', error);
+  }
+};
+
+main();
