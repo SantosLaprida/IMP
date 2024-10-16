eb2149f9f177c2c1a239f49a21afeec5c25fe4af
Carlos Laprida <89549449+Charlie1203@users.noreply.github.com>
2024-10-16 19:00:17 -0300

    fix

--- App.js
+++ App.js
@@ -30,196 +30,196 @@ const MainStack = createStackNavigator();
 const HomeStack = createStackNavigator();
 
 function AuthStackScreen() {
-  return (
-    <AuthStack.Navigator>
-      <AuthStack.Screen
-        name="Login"
-        component={Login}
-        options={{ headerShown: false }}
-      />
-      <AuthStack.Screen
-        name="Register"
-        component={Register}
-        options={{ headerShown: false }}
-      />
-      <AuthStack.Screen
-        name="PasswordReset"
-        component={PasswordReset}
-        options={{ headerShown: false }}
-      />
-    </AuthStack.Navigator>
-  );
+	return (
+		<AuthStack.Navigator>
+			<AuthStack.Screen
+				name="Login"
+				component={Login}
+				options={{ headerShown: false }}
+			/>
+			<AuthStack.Screen
+				name="Register"
+				component={Register}
+				options={{ headerShown: false }}
+			/>
+			<AuthStack.Screen
+				name="PasswordReset"
+				component={PasswordReset}
+				options={{ headerShown: false }}
+			/>
+		</AuthStack.Navigator>
+	);
 }
 
 function BetStackScreen() {
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen
-        name="Bets"
-        component={Bets}
-        options={{ headerShown: false }}
-      />
-    </HomeStack.Navigator>
-  );
+	return (
+		<HomeStack.Navigator>
+			<HomeStack.Screen
+				name="Bets"
+				component={Bets}
+				options={{ headerShown: false }}
+			/>
+		</HomeStack.Navigator>
+	);
 }
 
 function CustomTabBarButton({ children, onPress, isFocused }) {
-  return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={{
-        backgroundColor: isFocused ? "black" : "black",
-        borderColor: isFocused ? "#2296F3" : "grey",
-        height: 80,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 60,
-        marginHorizontal: 10,
-        width: 80,
-        position: "relative",
-        top: -20,
-        borderWidth: 3,
-      }}
-    >
-      {children}
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity
+			onPress={onPress}
+			style={{
+				backgroundColor: isFocused ? "black" : "black",
+				borderColor: isFocused ? "#2296F3" : "grey",
+				height: 80,
+				justifyContent: "center",
+				alignItems: "center",
+				borderRadius: 60,
+				marginHorizontal: 10,
+				width: 80,
+				position: "relative",
+				top: -20,
+				borderWidth: 3,
+			}}
+		>
+			{children}
+		</TouchableOpacity>
+	);
 }
 
 function BetTabBarButton(props) {
-  const isFocused = useIsFocused();
-  return <CustomTabBarButton {...props} isFocused={isFocused} />;
+	const isFocused = useIsFocused();
+	return <CustomTabBarButton {...props} isFocused={isFocused} />;
 }
 
 function TabNavigator() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          if (route.name === "Home") {
-            iconName = focused ? "home" : "home-outline";
-          } else if (route.name === "Tournaments") {
-            iconName = focused ? "trophy" : "trophy-outline";
-          } else if (route.name === "Bet") {
-            iconName = focused ? "cash" : "cash-outline";
-          } else if (route.name === "Wallet") {
-            iconName = focused ? "wallet" : "wallet-outline";
-          } else if (route.name === "Settings") {
-            iconName = focused ? "settings" : "settings-outline";
-          }
-          return <Ionicons name={iconName} size={22} color={color} />;
-        },
-        tabBarStyle: {
-          paddingBottom: 20,
-          paddingTop: 10,
-          height: 80,
-          backgroundColor: "black",
-          borderColor: "transparent",
-        },
-        tabBarLabelStyle: {
-          fontSize: 9,
-          fontFamily: "p-medium",
-        },
-      })}
-    >
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Home"
-        component={Home}
-      />
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Tournaments"
-        component={Tournaments}
-      />
-      <Tab.Screen
-        options={{
-          headerShown: false,
-          tabBarButton: (props) => <BetTabBarButton {...props} />,
-          tabBarIcon: ({ focused, color, size }) => (
-            <>
-              <Ionicons
-                name="cash"
-                size={30}
-                color={focused ? "#2296F3" : "grey"}
-              />
-              <Text
-                style={{
-                  color: focused ? "#2296F3" : "grey",
-                  fontFamily: "p-bold",
-                  fontSize: 12,
-                }}
-              >
-                Bet
-              </Text>
-            </>
-          ),
-          tabBarLabel: () => null, // Remove default label
-        }}
-        name="Bet"
-        component={BetStackScreen}
-      />
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Wallet"
-        component={Wallet}
-      />
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Settings"
-        component={Settings}
-      />
-    </Tab.Navigator>
-  );
+	return (
+		<Tab.Navigator
+			initialRouteName="Home"
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({ focused, color, size }) => {
+					let iconName;
+					if (route.name === "Home") {
+						iconName = focused ? "home" : "home-outline";
+					} else if (route.name === "Tournaments") {
+						iconName = focused ? "trophy" : "trophy-outline";
+					} else if (route.name === "Bet") {
+						iconName = focused ? "cash" : "cash-outline";
+					} else if (route.name === "Wallet") {
+						iconName = focused ? "wallet" : "wallet-outline";
+					} else if (route.name === "Settings") {
+						iconName = focused ? "settings" : "settings-outline";
+					}
+					return <Ionicons name={iconName} size={22} color={color} />;
+				},
+				tabBarStyle: {
+					paddingBottom: 20,
+					paddingTop: 10,
+					height: 80,
+					backgroundColor: "black",
+					borderColor: "transparent",
+				},
+				tabBarLabelStyle: {
+					fontSize: 9,
+					fontFamily: "p-medium",
+				},
+			})}
+		>
+			<Tab.Screen
+				options={{ headerShown: false }}
+				name="Home"
+				component={Home}
+			/>
+			<Tab.Screen
+				options={{ headerShown: false }}
+				name="Tournaments"
+				component={Tournaments}
+			/>
+			<Tab.Screen
+				options={{
+					headerShown: false,
+					tabBarButton: (props) => <BetTabBarButton {...props} />,
+					tabBarIcon: ({ focused, color, size }) => (
+						<>
+							<Ionicons
+								name="cash"
+								size={30}
+								color={focused ? "#2296F3" : "grey"}
+							/>
+							<Text
+								style={{
+									color: focused ? "#2296F3" : "grey",
+									fontFamily: "p-bold",
+									fontSize: 12,
+								}}
+							>
+								Bet
+							</Text>
+						</>
+					),
+					tabBarLabel: () => null, // Remove default label
+				}}
+				name="Bet"
+				component={BetStackScreen}
+			/>
+			<Tab.Screen
+				options={{ headerShown: false }}
+				name="Wallet"
+				component={Wallet}
+			/>
+			<Tab.Screen
+				options={{ headerShown: false }}
+				name="Settings"
+				component={Settings}
+			/>
+		</Tab.Navigator>
+	);
 }
 
 function MainStackScreen() {
-  return (
-    <MainStack.Navigator screenOptions={{ headerShown: false }}>
-      <MainStack.Screen name="Auth" component={AuthStackScreen} />
-      <MainStack.Screen name="Main" component={TabNavigator} />
-      <MainStack.Screen name="Players" component={Players} />
-      <MainStack.Screen name="QuarterFinals" component={QuarterFinals} />
-      <MainStack.Screen name="SemiFinals" component={SemiFinals} />
-      <MainStack.Screen name="Finals" component={Finals} />
-      <MainStack.Screen name="ThirdPlace" component={ThirdPlace} />
-      <MainStack.Screen name="Results" component={Results} />
-    </MainStack.Navigator>
-  );
+	return (
+		<MainStack.Navigator screenOptions={{ headerShown: false }}>
+			<MainStack.Screen name="Auth" component={AuthStackScreen} />
+			<MainStack.Screen name="Main" component={TabNavigator} />
+			<MainStack.Screen name="Players" component={Players} />
+			<MainStack.Screen name="QuarterFinals" component={QuarterFinals} />
+			<MainStack.Screen name="SemiFinals" component={SemiFinals} />
+			<MainStack.Screen name="Finals" component={Finals} />
+			<MainStack.Screen name="ThirdPlace" component={ThirdPlace} />
+			<MainStack.Screen name="Results" component={Results} />
+		</MainStack.Navigator>
+	);
 }
 
 export default function App() {
-  // Call useFonts at the top level of the functional component
-  const [fontsLoaded] = useFonts({
-    "p-bold": require("./assets/fonts/Poppins/Poppins-Bold.ttf"),
-    "p-semibold": require("./assets/fonts/Poppins/Poppins-SemiBold.ttf"),
-    "p-medium": require("./assets/fonts/Poppins/Poppins-Medium.ttf"),
-    "p-light": require("./assets/fonts/Poppins/Poppins-Light.ttf"),
-    "p-regular": require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
-    "p-italic": require("./assets/fonts/Poppins/Poppins-Italic.ttf"),
-  });
+	// Call useFonts at the top level of the functional component
+	const [fontsLoaded] = useFonts({
+		"p-bold": require("./assets/fonts/Poppins/Poppins-Bold.ttf"),
+		"p-semibold": require("./assets/fonts/Poppins/Poppins-SemiBold.ttf"),
+		"p-medium": require("./assets/fonts/Poppins/Poppins-Medium.ttf"),
+		"p-light": require("./assets/fonts/Poppins/Poppins-Light.ttf"),
+		"p-regular": require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
+		"p-italic": require("./assets/fonts/Poppins/Poppins-Italic.ttf"),
+	});
 
-  // Use effect to handle splash screen and font loading
-  React.useEffect(() => {
-    async function hideSplashScreen() {
-      if (fontsLoaded) {
-        await SplashScreen.hideAsync();
-      }
-    }
+	// Use effect to handle splash screen and font loading
+	React.useEffect(() => {
+		async function hideSplashScreen() {
+			if (fontsLoaded) {
+				await SplashScreen.hideAsync();
+			}
+		}
 
-    hideSplashScreen();
-  }, [fontsLoaded]);
+		hideSplashScreen();
+	}, [fontsLoaded]);
 
-  // While fonts are loading, return null
-  if (!fontsLoaded) {
-    return null;
-  }
+	// While fonts are loading, return null
+	if (!fontsLoaded) {
+		return null;
+	}
 
-  return (
-    <NavigationContainer>
-      <MainStackScreen />
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<MainStackScreen />
+		</NavigationContainer>
+	);
 }
--- server/firebaseConfig.js
+++ server/firebaseConfig.js
@@ -1,29 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth"; // Import the necessary functions for auth persistence
-import AsyncStorage from "@react-native-async-storage/async-storage"; // Import AsyncStorage for persistence
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXzPEQIqJA9mBoamIjsfAyptIjgpMyo1o",
-  authDomain: "internetmatchplay.firebaseapp.com",
-  projectId: "internetmatchplay",
-  storageBucket: "internetmatchplay.appspot.com",
-  messagingSenderId: "1030174523087",
-  appId: "1:1030174523087:web:00f3a877aabb57628981ba",
-  measurementId: "G-B7Q070NLJ3",
+	apiKey: "AIzaSyAXzPEQIqJA9mBoamIjsfAyptIjgpMyo1o",
+	authDomain: "internetmatchplay.firebaseapp.com",
+	projectId: "internetmatchplay",
+	storageBucket: "internetmatchplay.appspot.com",
+	messagingSenderId: "1030174523087",
+	appId: "1:1030174523087:web:00f3a877aabb57628981ba",
+	measurementId: "G-B7Q070NLJ3",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
-// Initialize Firestore
 const firestore = getFirestore(app);
-
-// Initialize Auth with persistence
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage), // Add persistence using AsyncStorage
-});
+const auth = getAuth(app);
 
 export { firestore, auth };
